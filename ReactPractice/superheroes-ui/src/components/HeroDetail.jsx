40d5076dbbe2437b4106d0883ab11c2ad2cd504d
Sarah Shaw <sarah.y.shaw.db@gmail.com>
2023-07-31 20:13:36 +0100

    Added basic react practice of batman

---
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import batman from '../images/batman_bongo_cat.jpeg'
+
+const HeroDetail = () => {
+  return (
+    <>
+        <div>HeroDetail</div>
+        <div>
+            <img src={batman} alt="Batman" height="250px"/>
+            <p id="blue-alias">Alias: Batman</p>
+            <p class="green-class">Name: Bruce Wayne</p>
+            <button class="green-class">Like</button>   
+        </div>
+    </>
+  )
+}
+
+export default HeroDetail;
