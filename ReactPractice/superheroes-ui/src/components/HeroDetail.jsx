27f86daaedaeabff2350a03bd9d877c208d7a095
Sarah Shaw <sarah.y.shaw.db@gmail.com>
2023-08-01 15:21:04 +0100

    Created basic map-generated components example

--- ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import batman from '../images/batman_bongo_cat.jpeg'
+import images from '..';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-const HeroDetail = () => {
+const HeroDetail = (props) => {
+  const {info} = props;
+  const hero = info;
+  // console.log("HeroDetails: info " + info + " key " + key);
+
   return (
-    <>
-        <div>HeroDetail</div>
-        <div>
-            <img src={batman} alt="Batman" height="250px"/>
-            <p id="blue-alias">Alias: Batman</p>
-            <p class="green-class">Name: Bruce Wayne</p>
-            <button class="green-class">Like</button>   
+      <Card>
+        <div className="imgContainer">
+          <Card.Img variant="top" src={images[props.info.alias.replaceAll(/\s/g, '')]} width="150px" />
         </div>
-    </>
+        <Card.Body>
+            <Card.Title id={hero.id}>Alias: {hero.alias}</Card.Title>
+            <Card.Text>Name: {hero.name}</Card.Text >
+            <Button variant="primary">Like</Button>   
+        </Card.Body>
+      </Card>
   )
 }
 
--- ReactPractice/superheroes-ui/src/index.js
+++ ReactPractice/superheroes-ui/src/index.js
@@ -4,6 +4,56 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// import batman from '../images/batman_bongo_cat.jpeg'
+// import h1 from '../images/1.jpeg';
+// import h2 from '../images/2.jpeg';
+// import h3 from '../images/3.jpeg';
+// import h4 from '../images/4.jpeg';
+// import h5 from '../images/5.jpeg';
+// import h6 from '../images/6.jpeg';
+// import h7 from '../images/7.jpeg';
+// import h8 from '../images/8.jpeg';
+// import h9 from '../images/9.jpeg';
+// import h10 from '../images/10.jpeg';
+
+import Batman from "./images/1.jpeg";
+import Superman from "./images/2.jpeg";
+import GreenLantern from "./images/4.jpeg";
+import TheHuntress from "./images/5.jpeg";
+import TheFlash from "./images/3.jpeg";
+import HarleyQuinn from "./images/6.jpeg";
+import Deadshot from "./images/7.jpeg";
+import CountVertigo from "./images/8.jpeg";
+import Robin from "./images/9.jpeg";
+import Nightwing from "./images/10.jpeg";
+
+const images = {
+    Batman,
+    Superman,
+    GreenLantern,
+    TheHuntress,
+    TheFlash,
+    HarleyQuinn,
+    Deadshot,
+    CountVertigo,
+    Robin,
+    Nightwing
+}
+
+// const hero = {
+//     "id":1,
+//     "name":"Bruce Wayne",
+//     "alias":"Batman",
+//     "superpower":"Martial Arts",
+//     "teamID":1
+// }
+
+// const images = {
+//   h1, h2, h3, h4, h5, h6, h7, h8, h9, h10
+// };
+
+export default images;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
